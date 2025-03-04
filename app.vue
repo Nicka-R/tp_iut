bb37b8f83b9501b42d026ef85c8e729b4bc037f1
Nicka-R <nicka.ratovobodo8@etu.univ-lorraine.fr>
2025-03-04 14:47:01 +0100

    feat : création de pages simples

--- app.vue
+++ app.vue
@@ -1,6 +1,51 @@
+<script setup lang="ts">
+import { NuxtPage } from '#components';
+
+</script>
+
 <template>
+  <header>    
+    <NuxtLink href="/bonjour">Bonjour</NuxtLink>
+    <NuxtLink href="/chat">Chat</NuxtLink>
+  </header>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+/* ajout de CSS */
+<style>
+  body{
+    margin: 0;
+    font-family: 'Franklin Gothic', 'Arial Narrow', Arial, sans-serif;
+  }
+  header{
+    background-color: rgb(240, 210, 255);
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+  }
+  main{
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 20px;
+    height: 100vh;
+  }
+
+  img{
+    width: 100%;
+    height: auto;
+  }
+
+  footer{
+    background-color: rgb(240, 210, 255);
+    p{
+      text-align: center;
+      padding: 10px;
+      margin: 0;
+    }
+  }
+
+</style>
---
+++ pages/bonjour.vue
@@ -0,0 +1,21 @@
+<template>
+<html>
+  <head>
+    <title>Page Bonjour</title>
+  </head>
+  <body>
+    <main>
+      <h1>Bonjour</h1>
+      <div class="page-content">
+        <span>Première page en Nuxt.js</span>
+        <img src="../assets/images/chien-prairie.jpg" alt="chien">
+      </div>
+    </main>
+    
+   <footer>
+      <p>TP IUT</p>
+      <p>Nicka Ratovobodo</p>
+    </footer>
+  </body>
+</html>
+</template>
